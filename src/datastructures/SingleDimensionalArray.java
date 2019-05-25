22944d037681964977fc101e41b5b82c753d8928
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2019-05-25 15:18:23 -0400

    Data Structures practice for Java

---
+++ src/datastructures/SingleDimensionalArray.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructures;
+
+/**
+ *
+ * @author al
+ */
+public class SingleDimensionalArray {
+    
+    int arr[] = null;
+
+    //Constructor
+    public SingleDimensionalArray(int arraySize) 
+    {
+        arr = new int[arraySize];
+    }
+    
+    
+    
+    
+    
+}
