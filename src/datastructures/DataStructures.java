f5354eb2ba88886c0e71c6317ac2f24f9b7a6414
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2019-05-25 16:00:40 -0400

    Making progress

--- src/datastructures/DataStructures.java
+++ src/datastructures/DataStructures.java
@@ -16,6 +16,28 @@ public class DataStructures {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        SingleDimensionalArray sda = new SingleDimensionalArray(10);
+        sda.PrintArray();
+        
+        System.out.println("");
+        System.out.println("");
+        
+        sda.Insert(0, 10);
+        sda.Insert(1, 200);
+        sda.Insert(2, 300);
+        sda.Insert(3, 400);
+        sda.Insert(3, 5000);
+        
+        System.out.println("");
+        
+        sda.PrintArray();
+        
+        System.out.println("");
+        System.out.println("");
+        
+        sda.Search(300); System.out.println("");
+        sda.PrintArrayCell(3);
     }
     
 }
--- src/datastructures/SingleDimensionalArray.java
+++ src/datastructures/SingleDimensionalArray.java
@@ -17,10 +17,88 @@ public class SingleDimensionalArray {
     public SingleDimensionalArray(int arraySize) 
     {
         arr = new int[arraySize];
+           
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.MIN_VALUE;
+        }
     }
     
+    //Displays Array
+    public void PrintArray()
+    {
+        try
+        {
+            for (int i = 0; i < arr.length; i++) 
+            {
+                System.out.print(arr[i] + " ");   
+            }
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            e.getStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.getStackTrace();
+        }
+    }
     
+    public void PrintArrayCell(int index)
+    {
+        try
+        {
+            System.out.println(arr[index]);
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            e.getStackTrace();
+        }
+    }
     
+    //Inserts value in the array
+    public void Insert(int index, int value)
+    {
+        try
+        {
+           if (arr[index] == Integer.MIN_VALUE)
+           {
+               arr[index] = value;
+               System.out.println(value + " was successfully inserted at index " + index);
+           }
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            e.getStackTrace();
+        }
+        catch(Exception e)
+        {
+            e.getStackTrace();
+        }
+    }
     
-    
+    //Search for item if exists
+    public void Search(int value)
+    {
+        try
+        {
+            for (int i = 0; i < arr.length; i++) 
+            {
+                if (arr[i] == value)
+                {
+                    System.out.println("Item exists");
+                    System.out.println(value + " has an index of " + i);
+                    return;
+                }
+            }
+            System.out.println(value + " not found!");
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            e.getStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.getStackTrace();
+        }
+    }
 }
